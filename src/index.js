ada585bc822bcae6c014aa77b9a16a8cbaa73f97
Jennifer Gómez <jengr42@gmail.com>
2023-03-14 18:36:37 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import simpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import {searchImages} from './indexImages';
 
